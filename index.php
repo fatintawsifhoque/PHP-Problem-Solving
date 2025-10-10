a72c720d2f139e1677bbe6cb43593a9f43a6d91e
Fatin Tawsif Hoque <fatintawsifhoque.fatin@gmail.com>
2025-10-10 16:34:19 +0600

    problem solving 6-9

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ function EvenOrOdd()
     }
 }
 EvenOrOdd();
-
+echo "<br> <br>";
 /*Problem -> 2 => Multiples of 3 or 5
 Using a for loop, print all numbers from 1 to 50 that are divisible by 3 or 5. If a number is divisible by both (e.g., 15, 30), print it only once. Do not print numbers that are not divisible by either.*/
 function multipleOf3Or5()
@@ -24,7 +24,7 @@ function multipleOf3Or5()
     }
 }
 multipleOf3Or5();
-
+echo "<br> <br>";
 /*Problem -> 3 => Factorial with Input Validation
 Ask the user for a number $n (you can hardcode it for testing, e.g., $n = 5).
 
@@ -44,21 +44,22 @@ function factorial($n)
     echo $factorial;
 }
 factorial(5);
-
+echo "<br> <br>";
 /*Problem -> 4 => Prime Number Finder (1 to n)
 Use a for loop to check every number from 1 to n. For each number, use nested loops and if...else logic to determine if it is a prime number. */
-function primeNumber($limit) {
+function primeNumber($limit)
+{
     for ($num = 2; $num <= $limit; $num++) {
         $isPrime = true;
-        
-    
+
+
         for ($i = 2; $i < $num; $i++) {
             if ($num % $i == 0) {
                 $isPrime = false;
                 break;
             }
         }
-        
+
         if ($isPrime) {
             echo "$num is prime<br>";
         }
@@ -66,19 +67,84 @@ function primeNumber($limit) {
 }
 
 primeNumber(30);
-
+echo "<br> <br>";
 /*Problem -> 5 => Conditional Star-Hash Pattern
 Print a right-angled triangle pattern with n rows using a for loop.
 
 On odd-numbered rows (1st, 3rd, 5th...), print *
 On even-numbered rows (2nd, 4th...), print #*/
-function starHash($limit){
- for ($i = 1; $i <= $limit; $i++) {
-        if ($i % 2 == 1) { 
+function starHash($limit)
+{
+    for ($i = 1; $i <= $limit; $i++) {
+        if ($i % 2 == 1) {
             echo str_repeat("*", $i) . "<br>";
-        } else { 
+        } else {
             echo str_repeat("#", $i) . "<br>";
         }
     }
 }
 starHash(5);
+echo "<br> <br>";
+/*Problem -> 6 => Sum of Even Numbers
+Write a PHP script that uses a for loop to calculate the sum of all even numbers from 1 to 100. Use an if statement to check if a number is even. Print the final sum.*/
+function sumOfEvenNum($n){
+    $sum =0;
+    for($i = 2;$i<=$n;$i+=2){
+        $sum+=$i;
+    }
+    echo $sum;
+}
+sumOfEvenNum(100);
+echo "<br> <br>";
+/*Problem -> 7 => Find the Largest Number in a Range with Condition
+Iterate from 100 to 200 using a for loop.
+For each number, check if it is divisible by 7 but not by 5.
+Among all such numbers, find and print the largest one.*/
+function largestNum($from, $to){
+    $largest =null;
+    for($i = $from;$i<=$to;$i++){
+        if($i % 7 == 0 && $i % 5 !==0){
+            if($largest<$i){
+                $largest =$i;
+            }
+        }
+    }
+    echo $largest;
+}
+largestNum(100,200);
+echo "<br> <br>";
+/*Problem -> 8 => Pattern with Numbers and Conditions
+Print a pattern of 5 lines using a for loop.
+
+On odd lines (1st, 3rd, 5th), print the line number repeated that many times (e.g., line 3 → 333).
+On even lines (2nd, 4th), print the word "SKIP".*/
+function patternWithNumAndCond() {
+    for ($i = 1; $i <= 5; $i++) {
+        if ($i % 2 == 0) {
+            echo "SKIP<br>";
+        } else {
+            for ($j = 1; $j <= $i; $j++) {
+                echo $i;
+            }
+            echo "<br>";
+        }
+    }
+}
+
+patternWithNumAndCond();
+echo "<br> <br>";
+/*Problem -> 9 => Count Vowels in a Fixed String Using Loop
+Define a string: $text = "The quick brown fox jumps over the lazy dog";
+Use a for loop to go through each character of the string.
+Use if...else (or nested if) to check if the character is a vowel (a, e, i, o, u — case-insensitive).*/
+function countVowel($str){
+    $str = strtolower($str);
+    $count = 0;
+    for($i = 0; $i<strlen($str);$i++){
+        if($str[$i] == "a" || $str[$i] == "e" || $str[$i] == "i" || $str[$i] == "o" || $str[$i] == "u"){
+            $count++;
+        }
+    }
+    echo $count;
+}
+countVowel("The quick brown fox jumps over the lazy dog");
