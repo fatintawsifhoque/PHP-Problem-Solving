2747669b51d3ce8fe02da6eb9c6ddfa6ec55b298
Fatin Tawsif Hoque <fatintawsifhoque.fatin@gmail.com>
2025-11-22 19:27:16 +0600

    55

---
+++ oop/NameSpaces/Controllers/User.php
@@ -0,0 +1,9 @@
+<?php
+namespace NameSpaces\Controllers;
+
+class User {
+    public function getType() {
+        return "Controller";
+    }
+}
+?>
---
+++ oop/NameSpaces/Models/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NameSpaces\Models;
+
+class User {
+    public function getType() {
+        return "Model";
+    }
+}
+?>
